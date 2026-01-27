08c42751388b30e01cca7f23c7529bb9507f5c81
Zaidon <zaidonaljbaae@gmail.com>
2026-01-26 23:29:56 -0300

    new version

--- infra/cdk/app.py
+++ infra/cdk/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import os
-
 import aws_cdk as cdk
 
 from stacks.ecs_stack import TemplateEcsStack
@@ -8,12 +7,29 @@ from stacks.ecs_stack import TemplateEcsStack
 app = cdk.App()
 
 stage = app.node.try_get_context("stage") or os.getenv("STAGE", "dev")
-region = app.node.try_get_context("region") or os.getenv("AWS_REGION", "us-east-1")
+
+region = (
+    app.node.try_get_context("region")
+    or os.getenv("CDK_DEFAULT_REGION")
+    or os.getenv("AWS_REGION", "us-east-1")
+)
+
+account = (
+    app.node.try_get_context("account")
+    or os.getenv("CDK_DEFAULT_ACCOUNT")
+    or os.getenv("AWS_ACCOUNT_ID")
+)
+
+if not account:
+    raise ValueError("AWS account is not defined. Set AWS_ACCOUNT_ID or CDK_DEFAULT_ACCOUNT")
 
 TemplateEcsStack(
     app,
     f"template-ecs-{stage}",
-    env=cdk.Environment(region=region),
+    env=cdk.Environment(
+        account=account,
+        region=region,
+    ),
 )
 
 app.synth()
